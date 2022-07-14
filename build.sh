958e5d4becb397f79c6c7cedd4a6c0cef7fb43c8
Patrik Pfaffenbauer <patrik.pfaffenbauer@live.at>
2022-07-14 11:36:09 +0200

    working on theme

--- build.sh
+++ build.sh
@@ -1,13 +1,2 @@
 #!/usr/bin/env bash
-
-# install dependencies
-npm install
-
-# compile theme
-npm run build
-
-# package theme and wrap it in a .jar file
-mvn package
-
-# move theme to /tmp/theme/ folder for futur use by keycloak
-mkdir -p /tmp/theme && cp -v target/*.jar /tmp/theme/
+npm install && npm run build && mvn package && mkdir -p /tmp/theme && cp -v target/*.jar /tmp/theme/
--- src/main/resources/theme/cloud-iam-redesign/email/html/password-reset.ftl
+++
@@ -1,6 +0,0 @@
-<#import "layoutCloudIAM.ftl" as layout>
-<@layout.layoutCloudIAM ; section>
-    <#if section = "text">
-        ${kcSanitize(msg("passwordResetBodyHtml",link, linkExpiration, realmName, linkExpirationFormatter(linkExpiration)))?no_esc}
-    </#if>
-</@layout.layoutCloudIAM>
